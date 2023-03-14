b9f7d9f1fa983d6f7fe4513e92d238236a42c411
Akash Shaw <112852873+shawakash@users.noreply.github.com>
2023-03-14 20:56:27 +0530

    Add

---
+++ news_hooks/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hooks from "./components/Hooks";
+import News from "./components/News";
+import UseEffectHooks from "./components/UseEffectHooks";
+import UserForm from "./components/UserForm";
+
+function App() {
+  return (
+    <>
+      <News />
+    </>
+  );
+}
+
+export default App;
